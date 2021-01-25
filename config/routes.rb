2fb0b28a75feee5ff016dc31b2620e25f5c674d5
Sarah Thiery <sarahvthiery@gmail.com>
2021-01-25 14:54:11 -0500

    Add seed data for restaurants, stores, amenities

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :comments
-  resources :stores, only: []
-  resources :restaurants, only: []
-  resources :amenities, only: []
+  resources :stores, only: [:index, :show]
+  resources :restaurants, only: [:index, :show]
+  resources :amenities, only: [:index, :show]
   resources :airports, only: [:index, :show]
   resources :users, only: []
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,13 +14,6 @@ Store.destroy_all
 # User.create(name: "Sarah")
 # User.create(name: "Johnson")
 
-# t.string "name"
-#     t.bigint "user_id", null: false
-#     t.string "city"
-#     t.string "country"
-#     t.integer "likes"
-#     t.string "comment"
-#     t.string "image"
 
 Airport.create(name: "LHR", city: "London", country: "United Kingdom", likes: 0, image: "https://www.cityam.com/wp-content/uploads/2020/02/London_Tower_Bridge_City.jpg")
 Airport.create(name: "SEA", city: "Seattle", country: "United States", likes: 0, image: "https://www.langan.com/wp-content/uploads/2020/01/Seattle-996x554-1.jpg")
@@ -34,9 +27,15 @@ Airport.create(name: "HKG", city: "Hong Kong", country: "Hong Kong" ,likes: 0, i
 Airport.create(name: "ORD", city: "Chicago", country: "United States",likes: 0, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQW8W1akvXFC6c6Z7A0DSas3HPAsX0Xwxq5Fw&usqp=CAU")
 Airport.create(name: "NRT", city: "Tokyo", country: "Japan",likes: 0, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxe0W46SQG-rVei6yoZluW59D66q7hHMzwNA&usqp=CAU")
 
+Amenity.create(airport_id: 1, name: "Water Fountain", service: "Water Fountain", likes: 0, image: "https://static.thenounproject.com/png/29-200.png")
+Amenity.create(airport_id: 1, name: "Prayer Room", service: "Prayer Room", likes: 0, image: "https://cdn3.iconfinder.com/data/icons/accomodation-outline/59/008_-_Prayer_Room-512.png")
 
+Restaurant.create(airport_id: 1, name: "McDonalds", cost: "$", cuisine: "American", likes: 0, image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/1170px-McDonald%27s_Golden_Arches.svg.png")
+Restaurant.create(airport_id: 1, name: "Starbucks", cost: "$", cuisine: "Coffee", likes: 0, image: "https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png")
 
+Store.create(airport_id: 1, name: "Boots", cost: "$", category: "Drugstore", image: "https://upload.wikimedia.org/wikipedia/en/thumb/d/d8/Boots_logo.svg/1200px-Boots_logo.svg.png")
 
+Store.create(airport_id: 1, name: "Sunglasses Hut", cost: "$$$", category: "Apparel", image: "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0002/3105/brand.gif?itok=fZZt-IgB")
 
 
 # restaurants
